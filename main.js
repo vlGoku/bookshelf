3c2ec808e541e85e7b25f08368a4b4af963c41e8
vlGoku <markolucic21@outlook.de>
2023-06-14 13:57:25 +0200

    second innit

--- main.js
+++ main.js
@@ -3,13 +3,22 @@
 const plus = document.querySelector(".plus");
 const addEntry = document.querySelector(".addEntry");
 const overlay = document.querySelector(".overlay");
+const close = document.querySelector(".close");
 
 function addBook() {
   plus.addEventListener("click", () => {
     addEntry.classList.add("showEntry");
     overlay.classList.add("showEntry");
+    close.classList.add("showEntry");
   });
 }
 
-function remove() {}
+function removeEntry() {
+  close.addEventListener("click", () => {
+    addEntry.classList.toggle("showEntry");
+    overlay.classList.toggle("showEntry");
+    close.classList.toggle("showEntry");
+  });
+}
 addBook();
+removeEntry();
