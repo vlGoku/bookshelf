ab508db9be526c50e080430a9789c94951de93e0
vlGoku <markolucic21@outlook.de>
2023-06-15 11:13:51 +0200

    small change 2.0

--- main.js
+++ main.js
@@ -3,21 +3,65 @@
 const plus = document.querySelector(".plus");
 const addEntry = document.querySelector(".addEntry");
 const overlay = document.querySelector(".overlay");
-const cancelOverlay = document.querySelector(".btn_cancel");
+const btnclose = document.querySelector(".btn_cancel");
+const btn_senden = document.querySelector(".btn_senden");
+
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const pages = document.querySelector("#pages");
+const genre = document.querySelector("#genre");
+const price = document.querySelector("#price");
+const read-status = document.querySelector("#read-status");
 
 function addBook() {
   plus.addEventListener("click", () => {
     addEntry.classList.add("showEntry");
     overlay.classList.add("showEntry");
+    btnclose.classList.add("showEntry");
   });
 }
 
-function cancelOverlay() {
-  cancelOverlay.addEventListener("click", () => {
-    addEntry.classList.toggle("hidden");
-    overlay.classList.toggle("hidden");
+function removeEntry() {
+  btnclose.addEventListener("click", () => {
+    addEntry.classList.remove("showEntry");
+    overlay.classList.remove("showEntry");
+    btnclose.classList.remove("showEntry");
   });
 }
-
 addBook();
-cancelOverlay();
+removeEntry();
+
+/* let library = []; */
+
+/* function Book(title, author, pages, genre, price,read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.genre = genre;
+  this.price = price;
+  this.read-status = read-status;
+} */
+
+/* function addBookToLibrary() {
+  const book = new Book(
+    title.value,
+    author.value,
+    parseInt(pages.value),
+    parseInt(price.value),
+    read-status.checked
+  );
+  library.push(book);
+  clearForm();
+} */
+
+/* function clearForm() {
+  title.value = "";
+  author.value = "";
+  pages.value = "";
+  genre.value = "";
+  price.value = "";
+  read-status.checked = false;
+} */
+
+/* btn_senden.addEventListener("click", addBookToLibrary); */
+
